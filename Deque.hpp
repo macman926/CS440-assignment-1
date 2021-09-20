95b5569e282ca89b943965eb18aa99a2978094f1
christianmcborrough@gmail.com <cmcborr1@binghamton.edu>
2021-09-20 00:29:31 -0400

    Set up constructors

--- Deque.hpp
+++ Deque.hpp
@@ -1,38 +1,50 @@
-
-#define DEQUE(X)
-
-struct Deque_##X{
-	//Struct fields
-	X* adt;
-	int front;
-	int back;
-	int size;
-	int total_elems;
-	char type_name[Deque_##X];
-
-	//Struct functions
-	size_t(*size)(Deque_##X);
-	bool(*empty)(Deque_##X);
-	void(*grow)(Deque_##X);
-	void(*push_back)(Deque_##X, X);
-	void(*push_front)(Deque_##X*, X);
-	X&(*front)(Deque_##X*);
-	X&(*back)(Deque_##X*);
-	void(*pop_back)(Deque_##X*);
-	void(*pop_front)(Deque_##X*);
-	Deque_##X##_Iterator(*begin)(Deque_##X*);
-	Deque_##X##_Iterator(*end)(Deque_##X*);
-	X&(*at)(Deque_##X*, size_t cursor);
-	void(*clear)(Deque_##X*);
-	void(*dtor)(Deque_##X*);
-	void(*sort)(Deque_##X*, Deque_##X##_Iterator, Deque)##X##_Iterator);
+#ifndef DEQUE
+#define DEQUE(X)	\
+			\
+struct Deque_##X{	\
+	X* adt;		\
+	int front;	\
+	int back;	\
+	int size;	\
+	int total_elems;\
+	\
+	size_t(*size)(Deque_##X);	\
+	bool(*empty)(Deque_##X);	\
+	void(*grow)(Deque_##X);		\
+	void(*clear)(Deque_##X);	\
+	void(*push_back)(Deque_##X, X);	\
+	void(*push_front)(Deque_##X*, X);	\
+	void(*pop_back)(Deque_##X*);	\
+	void(*pop_front)(Deque_##X*);	\
+	void(*clear)(Deque_##X*);	\
+	void(*dtor)(Deque_##X*);	\
+	void(*sort)(Deque_##X*, Deque_##X##_Iterator, Deque)##X##_Iterator);\
+	X&(*front)(Deque_##X*);\
+	X&(*back)(Deque_##X*);\
+	X&(*at)(Deque_##X*, size_t cursor);\
+	Deque_##X##_Iterator(*begin)(Deque_##X*);\
+	Deque_##X##_Iterator(*end)(Deque_##X*);\
+};	\
+void Deque_##X##_ctor(X##_Deque* deq, unsigned int (*fp)(unsigned int)){	\
+	deq->size = 10;	\
+	deq->total_elems = 0; \
+	deq->front = 0;\
+	deq->back = 0;\
+	deq->adt = (X*)(malloc)(sizeof(X)*10));\
+	deq->size = &X##_getSize;\
+	deq->empty = &X##_ifEmpty;\
+	deq->at = &X##_scoop;\
+	deq->push_front = &X##_pushf;\
+	deq->push_back = &X##_pushb;\
+	deq->pop_back = &X##_popb;\
+	deq->pop_front = &X##_popf;\
+	deq->clear = &X##_clearDeq;\
+	deq->front = &X##_getFront;\
+	deq->back = &X##_getBack;\
+}	\
+struct Deque_##X##_Iterator {	\
+	X&(*deref)(Deque_##X##_Iterator*);	\
+	void(*inc)(Deque_##X##_Iterator*);	\
+	void(*dec)(Deque_##X##_Iterator*);	\
 };
-
-struct Deque_##X##_Iterator {
-	X&(*deref)(Deque_##X##_Iterator*);
-	void(*inc)(Deque_##X##_Iterator*);
-	void(*dec)(Deque_##X##_Iterator*);
-};
-
-bool empty
 #endif
